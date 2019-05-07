eaeec645ab364050f64e59850c9b2d88200a9ed8
Jiri Mencak <jmencak@users.noreply.github.com>
2019-05-07 16:54:59 +0200

    Refactor to run as tests non-privileged user.
    - allow the HTTP tests to be run on cluster without admin privileges
    - remove the dependency on cluster loader and uses the "oc" client
    - remove the need to open port :9090 for workload generator pods
    - add support for copying results to other pods via k8s cp in addition to scp
    - remove the steps to setup Pbench; other tooling is responsible for this
    - make Pbench usage optional
    - make smoke tests faster by adjusting CL_TEMPLATES too
    - do not unnecessarily switch between projects
    - clean up old and unused code (JMeter variables, ...)

---
+++ wlg-run-pbench.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This is a wrapper script for pbench.
+
+main() {
+  # Show the configuration for the HTTP load generator
+  cat <<EOF
+RUN_TIME=$RUN_TIME
+MB_DELAY=$MB_DELAY
+MB_TARGETS=$MB_TARGETS
+MB_CONNS_PER_TARGET=$MB_CONNS_PER_TARGET
+MB_METHOD=$MB_METHOD
+MB_REQUEST_BODY_SIZE=$MB_REQUEST_BODY_SIZE
+MB_KA_REQUESTS=$MB_KA_REQUESTS
+MB_TLS_SESSION_REUSE=$MB_TLS_SESSION_REUSE
+URL_PATH=$URL_PATH
+LOAD_GENERATORS=$LOAD_GENERATORS
+EOF
+
+  test "$SERVER_RESULTS" && {
+    local server_results=$(echo ${SERVER_RESULTS:6} | cut -d: -f1)
+    SERVER_RESULTS="${SERVER_RESULTS:0:6}${server_results}:$benchmark_run_dir"
+  }
+
+  ./wlg-run.sh
+}
+
+main
